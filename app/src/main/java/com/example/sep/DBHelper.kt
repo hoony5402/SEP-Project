bcd34b77db0fea7895a5b4e9329745b55f30e06b
jisoo <jisukang@gm.gist.ac.kr>
2023-05-25 10:45:53 +0900

    missed DBHelper

---
+++ app/src/main/java/com/example/sep/DBHelper.kt
@@ -0,0 +1,29 @@
+package com.example.sep
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHelper(
+    context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+): SQLiteOpenHelper(context, name, factory, version) {
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL("CREATE TABLE if not exists posts(" +
+                "id integer primary key autoincrement,"+
+                "type text,"+
+                "title text,"+
+                "description text,"+
+                "date text,"+
+                "time text,"+
+                "location text,"+
+                "image text"+
+                ");")
+    }
+
+    override fun onUpgrade (db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE if exists posts")
+    }
+}
